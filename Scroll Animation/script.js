76ec480ad0f8a1d390fc5971c1c7bec0aad6e770
chkrishnamurthy <chalapatikrishna@gmail.com>
2021-01-22 23:37:32 +0530

    2 more projects done

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -1,1 +1,17 @@
-const boxes = document;
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkboxes);
+checkboxes();
+function checkboxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  //   const triggerBottom = window.innerHeight / 2;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    console.log(boxTop);
+    if (boxTop <= triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
