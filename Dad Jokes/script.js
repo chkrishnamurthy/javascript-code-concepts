53c71b5163392647afd3d2e436785bfbaf3b4717
chkrishnamurthy <chalapatikrishna@gmail.com>
2021-01-23 10:54:06 +0530

    dad jokes project done

---
+++ Dad Jokes/script.js
@@ -0,0 +1,19 @@
+//https://icanhazdadjoke.com/
+
+const jokeElement = document.querySelector("#joke");
+const jokeBtn = document.querySelector(".jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  const res = await fetch("https://icanhazdadjoke.com/", config);
+  const data = await res.json();
+  jokeElement.innerHTML = data.joke;
+}
